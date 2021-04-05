f2187a4e4ff8181a2028a3a7e1b2d68decc54559
Dev <ian.pa.vieira99@gmail.com>
2021-04-05 11:55:26 -0300

    add empreendedor

--- bd/bdgulaonline.sql
+++ bd/bdgulaonline.sql
@@ -15,5 +15,10 @@ create table tbempreendedor(
     cnpj varchar(18),
     linksite varchar(40) not null,
     email varchar(30) not null,
-    senha password(8) not null
+    senha varchar(8) not null
 );
+
+INSERT INTO tbempreendedor VALUES
+(1,"Ricardo Vasconcelos","123.456.567-99","1234-5678","jd pedro","Rua dos Pedregulhos","23","Sao Paulo","SP","62.461.140 /0008-90","www.gulaonlinecom.br","ricardo.vasconcelos@gmail.com.br","md5" ),
+(2,"Rodolfo Vasconcelos","123.987.567-99","1256-5678","jd petrolina","Rua dos Pesqueiros","28","Petrolina","PE","62.461.340 /0018-90","www.gulaonlinecom.br","rodolfo.vasconcelos@gmail.com.br","md5" ),
+(3,"Maria Vasconcelos","123.124.869-99","1902-5668","jd peri","Rua dos Abacateiros","89","Sao Paulo","SP","62.321.140 /0108-90","www.gulaonlinecom.br","maria.vasconcelos@gmail.com.br","md5" );
