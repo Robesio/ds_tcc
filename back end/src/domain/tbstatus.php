678c499d6fe916ea91b76fabcca390268a1c6dc0
Robesio <robesiodocarmo@gmail.com>
2021-04-16 09:27:02 -0300

    add os arquivos das 2 tabelas

---
+++ back end/src/domain/tbstatus.php
@@ -0,0 +1,102 @@
+<?php
+
+	class Tbstatus {
+		var $idstatus;
+		var $nomestatus;
+
+		function getIdstatus(){
+			return $this->idstatus;
+		}
+		function setIdstatus($idstatus){
+			$this->idstatus = $idstatus;
+		}
+
+		function getNomestatus(){
+			return $this->nomestatus;
+		}
+		function setNomestatus($nomestatus){
+			$this->nomestatus = $nomestatus;
+		}
+	}
+
+	class TbstatusDAO {
+		function create($tbstatus) {
+			$result = array();
+
+			try {
+				$query = "INSERT INTO table_name (column1, column2) VALUES (value1, value2)";
+
+				$con = new Connection();
+
+				if(Connection::getInstance()->exec($query) >= 1){
+				}
+
+				$con = null;
+			}catch(PDOException $e) {
+				$result["err"] = $e->getMessage();
+			}
+
+			return $result;
+		}
+
+		function read() {
+			$result = array();
+
+			try {
+				$query = "SELECT column1, column2 FROM table_name WHERE condition";
+
+				$con = new Connection();
+
+				$resultSet = Connection::getInstance()->query($query);
+
+				while($row = $resultSet->fetchObject()){
+				}
+
+				$con = null;
+			}catch(PDOException $e) {
+				$result["err"] = $e->getMessage();
+			}
+
+			return $result;
+		}
+
+		function update() {
+			$result = array();
+
+			try {
+				$query = "UPDATE table_name SET column1 = value1, column2 = value2 WHERE condition";
+
+				$con = new Connection();
+
+				$status = Connection::getInstance()->prepare($query);
+
+				if($status->execute()){
+				}
+
+				$con = null;
+			}catch(PDOException $e) {
+				$result["err"] = $e->getMessage();
+			}
+
+			return $result;
+		}
+
+		function delete() {
+			$result = array();
+
+			try {
+				$query = "DELETE FROM table_name WHERE condition";
+
+				$con = new Connection();
+
+				if(Connection::getInstance()->exec($query) >= 1){
+				}
+
+				$con = null;
+			}catch(PDOException $e) {
+				$result["err"] = $e->getMessage();
+			}
+
+			return $result;
+		}
+	}
