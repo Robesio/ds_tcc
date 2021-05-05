4155a609246b7206d70e4503c05d0a2f2e99f146
Robesio <robesiodocarmo@gmail.com>
2021-05-04 22:50:44 -0300

    banco de dados atualizado

--- bd/dbgulaonline.sql
+++ bd/dbgulaonline.sql
@@ -15,8 +15,6 @@ CREATE TABLE tbcadastro (
   cnpj VARCHAR(18) NULL,
   linksite VARCHAR(30) NULL,
   email VARCHAR(40) NOT NULL,
-  tipo VARCHAR(12) NOT NULL,
-  senha VARCHAR(14) NOT NULL,
   PRIMARY KEY(id_ca)
 );
 
@@ -63,53 +61,9 @@ CREATE TABLE tbali_has_tbped (
   idali INTEGER(4) NOT NULL,
   idpedido INTEGER(4) NOT NULL,
   qtdcli INTEGER(4) NOT NULL,
-  PRIMARY KEY(idali, idpedido),
   constraint fk_tbinter foreign key (idali) references tbalimentos(idali)ON DELETE CASCADE ON UPDATE CASCADE,
   constraint fk_tblig foreign key (idpedido) references tbpedido(idpedido)ON DELETE CASCADE ON UPDATE CASCADE
 );
-
-INSERT INTO tbcadastro VALUES
-(1,"Ricardo Vasconcelos","99059848012","1234-5678","jd pedro","Rua dos Pedregulhos","23",
-"Sao Paulo","SP","66356352000192","docesalgado.com","ricardo@gmail.com.br","Empreendedor"),
-(2,"Rodolfo Vasconcelos","56324164020","1256-5678","jd petrolina","Rua dos Pesqueiros","28",
-"Petrolina","PE","76386701000172","salgadobom.com","rodolfo@gmail.com.br","Empreendedor"),
-(3,"Maria Vasconcelos","51954853050","1902-5668","jd peri","Rua dos Abacateiros","89",
-"Sao Paulo","SP","14357367000171","docedoce.com","maria@gmail.com.br","Comum");
-
-INSERT INTO tbtipos_logins VALUES(1,"Empreendedor"),(2,"Comum");
-
-INSERT INTO tbstatus VALUES(1,"Realizado"),(2,"Enviado"),(3,"Entregue");
-
-INSERT INTO tbpedido VALUES
-(1,1,"Robésio","98989-8989","Rua Nova","234 B","Jardim Bom",CURRENT_TIMESTAMP(),1),
-(2,1,"Robésio","98989-8989","Rua Nova","234 B","Jardim Bom",CURRENT_TIMESTAMP(),1),
-(3,1,"Ian","97887-0000","Rua Amanco","333","Jardim do Café",CURRENT_TIMESTAMP(),1),
-(4,2,"Ian","97887-0000","Rua Amanco","333","Jardim do Café",CURRENT_TIMESTAMP(),1),
-(5,2,"Maisa","91212-1212","Rua Pedreira","444","Jardim Pedreira",CURRENT_TIMESTAMP(),2),
-(6,2,"Maisa","91212-1212","Rua Pedreira","444","Jardim Pedreira",CURRENT_TIMESTAMP(),2),
-(7,3,"Janderson","93333-5555","Rua Piaui","111","Jardim Novo",CURRENT_TIMESTAMP(),3),
-(8,3,"Janderson","93333-5555","Rua Piaui","111","Jardim Novo",CURRENT_TIMESTAMP(),3),
-(9,3,"Janderson","93333-5555","Rua Piaui","111","Jardim Novo",CURRENT_TIMESTAMP(),3);
-
-INSERT INTO tbalimentos VALUES
-(1,1,"Coxinha","Frango",1,5,""),
-(2,1,"Pastel","Carne",1,10,""),
-(3,1,"Quibe","Salsicha",1,5,""),
-(4,2,"Brigadeiro","Chocolate branco",50,25,""),
-(5,2,"Bolo de Pote","Balmilha",1,10,""),
-(6,2,"Pudim","Pudim",1,15,""),
-(7,3,"Marmita","Com Frango",1,25,""),
-(8,3,"Marmita","Com Carne Bovina",1,30,""),
-(9,3,"Marmita","Com Ovo",1,15,"");
-
-INSERT INTO tbali_has_tbped VALUES(1,1,2.2),(2,1,10),(3,1,20),(4,2,15),(5,2,9),(6,3,4);
-
-CREATE VIEW vw_usuario as
-SELECT  c.id_ca, c.nome, c.cpf, c.fone, c.bairro, c.rua, c.numero, c.cidade, c.uf, c.cnpj, c.linksite, c.email, l.senha
-FROM tbcadastro c LEFT JOIN tblogins l on (c.id_ca = l.id_ca);
-
-
-
 
 CREATE TABLE tblogins (
   id_ca INTEGER(4) NOT NULL,
@@ -120,10 +74,44 @@ CREATE TABLE tblogins (
   constraint fk_tblog foreign key (idti_lo) references tbtipos_logins(idti_lo) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+INSERT INTO tbcadastro VALUES
+(1,"Gula Online","76562399017","1111-1111","JD Gulozeimas","Rua da Gula","100",
+"Sao Paulo","SP","","","gulaonline@.com.br"),
+(2,"Ricardo Vasconcelos","99059848012","1234-5678","jd pedro","Rua dos Pedregulhos","23",
+"Sao Paulo","SP","66356352000192","docesalgado.com","ricardo@gmail.com.br"),
+(3,"Rodolfo Vasconcelos","56324164020","1256-5678","jd petrolina","Rua dos Pesqueiros","28",
+"Petrolina","PE","76386701000172","salgadobom.com","rodolfo@gmail.com.br"),
+(4,"Maria Vasconcelos","51954853050","1902-5668","jd peri","Rua dos Abacateiros","89",
+"Sao Paulo","SP","","","maria@gmail.com.br");
 
-INSERT INTO tblogins VALUES(1,1,md5("11")),(2,2,md5("22")),(3,2,md5("33"));
+INSERT INTO tbtipos_logins VALUES(1,"GulaOnline"),(2,"Empreendedor"),(3,"Comum");
+
+INSERT INTO tbstatus VALUES(1,"Realizado"),(2,"Enviado"),(3,"Entregue");
+
+INSERT INTO tbpedido VALUES
+(1,2,"Robésio","98989-8989","Rua Nova","234 B","Jardim Bom",CURRENT_TIMESTAMP(),1),
+(2,2,"Ian","97887-0000","Rua Amanco","333","Jardim do Café",CURRENT_TIMESTAMP(),1),
+(3,3,"Maisa","91212-1212","Rua Pedreira","444","Jardim Pedreira",CURRENT_TIMESTAMP(),2);
+
+INSERT INTO tbalimentos VALUES
+(1,2,"Coxinha","Frango",1,5,""),
+(2,2,"Pastel","Carne",1,10,""),
+(3,2,"Quibe","Salsicha",1,5,""),
+(4,3,"Brigadeiro","Chocolate branco",50,25,""),
+(5,3,"Bolo de Pote","Balmilha",1,10,""),
+(6,3,"Pudim","Pudim",1,15,"");
+
+INSERT INTO tbali_has_tbped VALUES(1,1,20),(2,1,10),(3,2,20),(5,3,9),(6,3,9);
+
+INSERT INTO tblogins VALUES(1,1,"11"),(2,2,"22"),(3,2,"33"),(4,3,"44");
 
 
---CREATE VIEW vw_nota_pedido as
---SELECT n.idpedido, a.idali, a.nomeali, p.nomecli, p.fonecli, p.enderecocli, p.numerocli, p.bairrocli,  preco * qtd AS valor_total, n.datahora
---FROM tbalimentos a LEFT JOIN tbali_ped p on (a.idali = p.idali) LEFT JOIN tbpedidos_nota n on (p.idpedido = n.idpedido);
+
+CREATE VIEW vw_usuario as
+SELECT  c.id_ca, c.nome, c.cpf, c.fone, c.bairro, c.rua, c.numero, c.cidade, c.uf, c.cnpj, c.linksite, c.email, l.senha, l.idti_lo
+FROM tbcadastro c LEFT JOIN tblogins l on (c.id_ca = l.id_ca);
+
+CREATE VIEW vw_pedidos as
+SELECT p.id_ca, p.idpedido, h.idali, p.nomecli, p.fonecli, ruacli, p.numerocli, p.bairrocli, p.datahora, p.idstatus, h.qtdcli
+FROM tbpedido p LEFT JOIN tbali_has_tbped h on (p.idpedido = h.idpedido);
+
---
+++ mb/gulaonlineapp/appgulaonline/src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'folder/Inbox',
+    pathMatch: 'full'
+  },
+  {
+    path: 'folder/:id',
+    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./paginas/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'cadastro',
+    loadChildren: () => import('./paginas/cadastro/cadastro.module').then( m => m.CadastroPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./paginas/login/login.module').then( m => m.LoginPageModule)
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
